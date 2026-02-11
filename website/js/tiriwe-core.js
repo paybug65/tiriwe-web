cb7beb1672639de350f79ed6e35b10a66ca82af3
paybug65 <paybug@gmail.com>
2026-02-11 14:26:09 +1300

    Update tiriwe-core.js

--- website/js/tiriwe-core.js
+++ website/js/tiriwe-core.js
@@ -1,1 +1,8 @@
-
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/umd/supabase.js"></script>
+<script src="js/tiriwe-core.js"></script>
+<script>
+    Tiriwe.ready().then(() => {
+        const user = Tiriwe.user;
+        // page-specific code here
+    });
+</script>
